ed61506c70999c4beebd3596b5ae1383535c7ae7
tws2xa <tws2xa@virginia.edu>
2015-10-02 14:44:06 -0400

    Cards look better

--- js/Card.js
+++ js/Card.js
@@ -1,38 +1,81 @@
-function Card(type, text, pageNum) {
+function Card(type, text, pageStart, pageEnd) {
 
 	this.type = type;
 	this.text = text;
-	this.pageNum = pageNum;
+	this.pageNum = [pageStart, pageEnd];
 
 	this.x = 10;
 	this.y = 10;
 
-	this.width = 150;
-	this.height = 200;
+	this.width = 180;
+	this.height = 240;
 
-	this.hBuffer = 3;
+	this.hBuffer = 15;
+	this.topBuffer = 0;
+	this.bottomBuffer = 10;
 
-	this.titleFontSize = 20;
+	this.titleFontName = "segoe ui semibold"
+	this.titleFontSize = 24;
+
+	this.bodyFontName = "segoe ui";
 	this.normalFontSize = 12;
 
-	this.backColor = "#FF0000";
-	this.textColor = "#000000";
+	this.backColor = "#FCE694";
+	this.textColor = "#373737";
+}
+
+Card.prototype.drawTitleText = function(context) {
+	context.textAlign = "center";
+	context.textBaseline="top";
+	context.font = ("bold " + this.titleFontSize + "px " + this.titleFontName);
+	context.fillStyle = this.textColor;
+	context.fillText(this.type, this.getCenter()[0], this.y + this.topBuffer, this.width);
+}
+
+Card.prototype.drawBodyText = function(context) {
+	context.textAlign = "start";
+	context.textBaseline="top";
+	context.font = ("normal " + this.normalFontSize + "px " + this.bodyFontName);
+	context.fillStyle = this.textColor;
+	wrapText(context, this.text, this.x + this.hBuffer,
+		this.y  + this.topBuffer + this.titleFontSize*1.5, 
+		this.width - this.hBuffer * 2, 
+		this.normalFontSize * 1.5);
+}
+
+Card.prototype.drawPageNumbers = function(context) {
+	if(this.pageNum == [-1, -1]) {
+		return;
+	}
+	else if(this.pageNum[0] == -1 || this.pageNum[1] == -1) {
+		pgText = "p. " + Math.max(this.pageNum[0], this.pageNum[1]);
+	} else {
+		pgText = "pp. " + this.pageNum[0] + " - " + this.pageNum[1];
+	}
+
+	context.textAlign = "end";
+	context.textBaseline="bottom";
+	context.font = ("normal " + this.normalFontSize + "px " + this.bodyFontName);
+	context.fillStyle = this.textColor;
+	context.fillText(pgText,
+		this.x + this.width - this.hBuffer,
+		this.y + this.height - this.bottomBuffer);
 }
 
 Card.prototype.draw = function(context) {
 	context.fillStyle = this.backColor;
 	context.fillRect(this.x, this.y, this.width, this.height);
+	//context.drawImage()
 
-	// Set font
-	context.textBaseline='top';
-	context.font = ("bold " + this.titleFontSize + "px Georgia");
-	context.fillStyle = this.textColor;
-	context.fillText(this.type, this.x + this.hBuffer, this.y, this.width);
-	context.font = (this.normalFontSize + "px Georgia");
-	wrapText(context, this.text, this.x + this.hBuffer, this.y  + this.titleFontSize*1.5, this.width, this.normalFontSize * 1.5);
+	// Text
+	this.drawTitleText(context);
+	this.drawBodyText(context);
+	this.drawPageNumbers(context);
 }
 
-
+Card.prototype.getCenter = function() {
+	return [this.x + this.width / 2, this.y + this.height / 2];
+}
 
 function wrapText(context, text, x, y, maxWidth, lineHeight) {
 	var words = text.split(' ');
--- js/playGame.js
+++ js/playGame.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 	var canvas = $("#canvas")[0];
 	var context = canvas.getContext("2d");
 
-	var canvasWidth = window.innerWidth * 0.99;
-	var canvasHeight = window.innerHeight * 0.98;
+	var canvasWidth = window.innerWidth * 0.97;
+	var canvasHeight = window.innerHeight * 0.97;
 
 	setCanvasSize();
 	init();
@@ -26,7 +26,7 @@ $(document).ready(function() {
 		// Draw Card
 		var card = new Card("Type", "Here is a bunch of text. Maybe it's a quote? Maybe it's some imagery? "
 			+ "Whatever the case, I'm sure it's great, because this student used Bookmark to learn all about "
-			+ "how to read critically!", 5);
+			+ "how to read critically!", 5, 10);
 		card.draw(context);
 	}
 });
