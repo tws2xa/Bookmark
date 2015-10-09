be2cb4eefe85717de0f2de74146254c775f97a56
enwint <enwinters@comcast.net>
2015-10-09 08:28:04 -0400

    cards display statically

--- js/ArgumentDisplay.js
+++ js/ArgumentDisplay.js
@@ -2,8 +2,61 @@ function ArgumentDisplay(x, y, width, height) {
 	this.position = new Rectangle(x,y,width,height);
 	this.backColor = "#FCE694";	
 	this.shadowColor = "#B9A875"
+	this.cardLinkColor = "#373737";
 	this.shadowSize = 2;
+
+	this.selectedShadowAddition = 0.5; // The amount to increase the selected card's shadow.
+	this.selectedScaleAddition = 0; // The amount to increase the selected card's scale.
+
+	this.selectedCard = null;
+	this.selectedCardPointerOffset = [0, 0];
+
+	this.newLinkStartCard = null;
+	this.linkSize = 2;
+	this.dragMousePos = [0, 0];
+
+	this.defaultCardScale = 1;
+	this.scaleChangeAmt = 0.1; // How much to change scale on mouse wheel
+	this.scaleMin = 0.2;
+	this.scaleMax = 1.5;
+
+	this.cards = [];
+	this.cardLinks = [];
+	
+	var card = new Card(
+			0,
+			"Argument",
+		"Hazel's loyalty is his most important quality.",
+		-1,
+		-1,
+		x + width - 215,
+		y + height / 2 - 40,
+		170,
+		125
+		);
+	var card2 = new Card(
+		2,
+		"Argument",
+		"Bigwig is a very brave bunny.",
+		-1,
+		-1,
+		x + width - 215,
+		y + height / 2 - 170,
+		170,
+		125
+		)
+
+	this.addCard(card);
+	this.addCard(card2);
+	
+	
+
 }
+
+
+//-------------------------------------------------------------
+//-----------------------Draw Functions------------------------
+//-------------------------------------------------------------
 
 ArgumentDisplay.prototype.drawShadow = function(context) {
 	context.fillStyle = this.shadowColor;
@@ -25,47 +78,240 @@ ArgumentDisplay.prototype.drawShadow = function(context) {
 		this.position.y + this.position.height,
 		this.position.width + this.shadowSize * 2,
 		this.shadowSize * 2);
-	/*
-	context.fillRect(
-		this.position.x + this.position.width,
-		this.position.y,
-		this.shadowSize,
-		this.position.height + 1);
-	context.fillRect(
-		this.position.x,
-		this.position.y + this.position.height,
-		this.position.width + this.shadowSize,
-		this.shadowSize);
-	*/
 }
 
 ArgumentDisplay.prototype.draw = function(context){
 	context.fillStyle = this.backColor;
 	context.fillRect(this.position.x, this.position.y, this.position.width, this.position.height);
-	this.drawShadow(context)	
+	this.drawShadow(context)
+
+	this.drawLinks(context);
+
+	for(var cardNum = 0; cardNum < this.cards.length; cardNum++) {
+		this.cards[cardNum].draw(context);
+	}
 }
 
-ArgumentDisplay.prototype.mouseClick=function(event){
-	console.log(event.clientX - this.position.x);
-	console.log(event.clientY - this.position.y);
+ArgumentDisplay.prototype.drawLinks = function(context) {
+	for(var linkNum = 0; linkNum < this.cardLinks.length; linkNum++) {
+		var cardLink = this.cardLinks[linkNum];
+		var cardPair = [];
+
+		for(var cardNum = 0; cardNum < this.cards.length; cardNum++) {
+			var id = this.cards[cardNum].uniqueId;
+			if(id == cardLink[0] || id == cardLink[1]) {
+				cardPair.push(this.cards[cardNum]);
+				if(cardPair.length >= 2) {
+					break;
+				}
+			}
+		}
+
+		if(cardPair.length == 2) {
+			this.drawLink(cardPair[0].getRealPosition().getCenter(), cardPair[1].getRealPosition().getCenter(), context);
+		}
+	}
+
+	if(this.newLinkStartCard != null) {
+		this.drawLink(this.newLinkStartCard.getRealPosition().getCenter(), this.dragMousePos, context);
+		context.fillStyle = this.cardLinkColor;
+		context.fillRect(this.dragMousePos[0] - this.linkSize, this.dragMousePos[1] - this.linkSize,
+			this.linkSize * 2, this.linkSize * 2);
+	}
+}
+
+
+//-------------------------------------------------------------
+//-----------------------Mouse Listeners-----------------------
+//-------------------------------------------------------------
+
+ArgumentDisplay.prototype.mouseClick=function(e, canvasRect){
+	e.preventDefault();
 }
 
 ArgumentDisplay.prototype.onMouseDown = function(e, canvasRect) {
+	e.preventDefault();
 
+	var xClickPos = (event.clientX - canvasRect.left);
+	var yClickPos = (event.clientY - canvasRect.top);
+	this.dragMousePos = [xClickPos, yClickPos];
+	
+	// Loop backwards so that with overlapping cards,
+	// we select the card on top (which feels more natural).
+	for(var cardNum = this.cards.length - 1; cardNum >= 0 ; cardNum--) {
+		if(this.cards[cardNum].getRealPosition().contains(xClickPos, yClickPos)) {
+			if(e.which == 1) { // Left Click
+				this.selectCard(cardNum, [xClickPos, yClickPos]);
+				break;
+			} else if (e.which == 3) { // Right Click
+				this.newLinkStartCard = this.cards[cardNum];
+			}
+		}
+	}	
 }
 
 ArgumentDisplay.prototype.onMouseUp = function(e, canvasRect) {
+	e.preventDefault();
+	
+	if(this.selectedCard != null && e.which == 1) {
+		this.clearSelectedCard();	
+	} if(this.newLinkStartCard != null && e.which == 3) {
+		
+		var xClickPos = (event.clientX - canvasRect.left);
+		var yClickPos = (event.clientY - canvasRect.top);
 
+		var newLinkEndCard = null;
+
+		// Loop backwards so that with overlapping cards,
+		// we select the card on top (which feels more natural).
+		for(var cardNum = this.cards.length - 1; cardNum >= 0 ; cardNum--) {
+			if(this.cards[cardNum].uniqueId == this.newLinkStartCard.uniqueId) {
+				continue;
+			}
+			if(this.cards[cardNum].getRealPosition().contains(xClickPos, yClickPos)) {
+				newLinkEndCard = this.cards[cardNum];
+				break;
+			}
+		}
+
+		if(newLinkEndCard != null) {
+			this.addCardLink(this.newLinkStartCard, newLinkEndCard);
+		}
+
+		this.newLinkStartCard = null;	
+	}
 }
 
 ArgumentDisplay.prototype.onMouseDrag = function(e, canvasRect) {
-	var xClickPos = event.clientX - canvasRect.left;
-	var yClickPos = event.clientY - canvasRect.top;
+	e.preventDefault();
 
-	console.log("Args Mouse Drag Pos: (" + xClickPos + ", " + yClickPos + ")!");
+	var xClickPos = (event.clientX - canvasRect.left);
+	var yClickPos = (event.clientY - canvasRect.top);
+	this.dragMousePos = [xClickPos, yClickPos];
+
+	if(this.selectedCard != null) {
+		var newCardX = xClickPos + this.selectedCardPointerOffset[0];
+		var newCardY = yClickPos + this.selectedCardPointerOffset[1];
+
+		this.selectedCard.moveTo(newCardX, newCardY, this.position);
+	}
 }
 
 ArgumentDisplay.prototype.onMouseWheel = function(e, canvasRect) {
 	var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-	console.log("ArgumentDisplay Wheel: " + delta);
+	this.adjustScale(delta * this.scaleChangeAmt);
+}
+
+//-------------------------------------------------------------
+//----------------------Helper Functions-----------------------
+//-------------------------------------------------------------
+
+ArgumentDisplay.prototype.addCard = function(card) {
+	card.scale = this.defaultCardScale;
+	this.cards.push(card);
+}
+
+ArgumentDisplay.prototype.selectCard = function(cardIndex, pointerPos) {
+	if(this.selectedCard != null) {
+		this.clearSelectedCard();
+	}
+
+	var card = this.cards[cardIndex];
+
+	// Move selected card to back of the cards list (so it always appears on top)
+	this.cards[cardIndex] = this.cards[this.cards.length - 1];
+	this.cards[this.cards.length - 1] = card;
+	
+	this.selectedCard = card;
+	this.selectedCardPointerOffset = [card.getScaledXPos() - pointerPos[0], card.getScaledYPos() - pointerPos[1]];
+	card.shadowSize += this.selectedShadowAddition;
+	card.scale += this.selectedScaleAddition;
+}
+
+ArgumentDisplay.prototype.clearSelectedCard = function() {
+	if(this.selectedCard != null) {
+		this.selectedCard.shadowSize -= this.selectedShadowAddition;
+		this.selectedCard.scale -= this.selectedScaleAddition;
+		this.selectedCard = null;
+		this.selectedCardPointerOffset = [0, 0];
+	}
+}
+
+ArgumentDisplay.prototype.adjustScale = function(amt) {
+	// Check bounds
+	var newScale = this.defaultCardScale + amt;
+	if(newScale > this.scaleMax || newScale < this.scaleMin) {
+		return;
+	}
+
+	this.defaultCardScale = newScale;
+	for(var cardNum=0; cardNum<this.cards.length; cardNum++) {
+		//var pos = this.cards[cardNum].getRealPosition();
+		this.cards[cardNum].scale += amt;
+		//this.cards[cardNum].moveTo(pos.x, pos.y, null);
+	}
+}
+
+ArgumentDisplay.prototype.addCardLink = function(start, end){
+
+	// Check it isn't a duplicate link
+	for(var linkNum = 0; linkNum < this.cardLinks.length; linkNum++) {
+		var cardLink = this.cardLinks[linkNum];
+		if( (cardLink[0] == start.uniqueId && cardLink[1] == end.uniqueId)
+			|| (cardLink[0] == end.uniqueId && cardLink[1] == start.uniqueId) ) {
+			console.log("Link already exists.");
+			return;
+		}
+	}
+
+	this.cardLinks.push([start.uniqueId, end.uniqueId]);
+}
+
+ArgumentDisplay.prototype.drawLink = function(center1, center2, context) {
+	
+	/*
+	 * Straight Line
+	 */
+	context.beginPath();
+	context.moveTo(center1[0], center1[1]);
+	context.lineTo(center2[0], center2[1]);
+	context.lineWidth = this.linkSize;
+	context.stroke();
+
+
+	/**
+	 * Horizontal and Vertical Lines Only
+	 *
+	// Sort left from right
+	var leftCardCenter;
+	var rightCardCenter;
+	if(center1[0] < center2[0]) {
+		leftCardCenter = center1;
+		rightCardCenter = center2;
+	} else {
+		leftCardCenter = center2;
+		rightCardCenter = center1;
+	}
+
+	// Draw the link
+	context.fillStyle = this.cardLinkColor;
+
+	// Horizontal Component
+	context.fillRect(
+		leftCardCenter[0],
+		leftCardCenter[1] - this.linkSize / 2,
+		rightCardCenter[0] - leftCardCenter[0],
+		this.linkSize
+	);
+
+	// Vertical Component
+	context.fillRect(
+		rightCardCenter[0] - this.linkSize / 2,
+		leftCardCenter[1],
+		this.linkSize,
+		rightCardCenter[1] - leftCardCenter[1]
+	);
+	 *
+	 *
+	**/
 }
--- js/DeckDisplay.js
+++ js/DeckDisplay.js
@@ -1,9 +1,72 @@
 function DeckDisplay(x, y, width, height) {
-	this.backColor = "#FCE694";	
 	this.position = new Rectangle(x,y,width,height);
+	this.backColor = "#FCE694";	
 	this.shadowColor = "#B9A875"
+	this.cardLinkColor = "#373737";
 	this.shadowSize = 2;
+
+	this.selectedShadowAddition = 0.5; // The amount to increase the selected card's shadow.
+	this.selectedScaleAddition = 0; // The amount to increase the selected card's scale.
+
+	this.selectedCard = null;
+	this.selectedCardPointerOffset = [0, 0];
+
+	this.newLinkStartCard = null;
+	this.linkSize = 2;
+	this.dragMousePos = [0, 0];
+
+	this.defaultCardScale = 1;
+	this.scaleChangeAmt = 0.1; // How much to change scale on mouse wheel
+	this.scaleMin = 0.2;
+	this.scaleMax = 1.5;
+
+	this.cards = [];
+	this.cardLinks = [];
+var card = new Card(
+			0,
+			"Imagery",
+			"\"A dazzling claw of lightning streaked down the length of the sky.\"",
+			362,
+			-1,
+			x + 30,
+			y + 20,
+			125,
+			170
+		);
+	var card2 = new Card(
+			1,
+			"Tone",
+			"Intense Fear",
+			-1,
+			-1,
+			x + 330,
+			y + 20,
+			125,
+			170
+		);
+
+	var card3 = new Card(
+		2,
+		"Tone",
+		"Triumph",
+		-1,
+		-1,
+		
+			x + 180,
+			y + 20,
+			125,
+			170
+		)
+
+	this.addCard(card);
+	this.addCard(card2);
+	this.addCard(card3);
 }
+
+
+//-------------------------------------------------------------
+//-----------------------Draw Functions------------------------
+//-------------------------------------------------------------
 
 DeckDisplay.prototype.drawShadow = function(context) {
 	context.fillStyle = this.shadowColor;
@@ -25,47 +88,240 @@ DeckDisplay.prototype.drawShadow = function(context) {
 		this.position.y + this.position.height,
 		this.position.width + this.shadowSize * 2,
 		this.shadowSize * 2);
-	/*
-	context.fillRect(
-		this.position.x + this.position.width,
-		this.position.y,
-		this.shadowSize,
-		this.position.height + 1);
-	context.fillRect(
-		this.position.x,
-		this.position.y + this.position.height,
-		this.position.width + this.shadowSize,
-		this.shadowSize);
-	*/
 }
 
 DeckDisplay.prototype.draw = function(context){
 	context.fillStyle = this.backColor;
 	context.fillRect(this.position.x, this.position.y, this.position.width, this.position.height);
-	this.drawShadow(context);	
+	this.drawShadow(context)
+
+	this.drawLinks(context);
+
+	for(var cardNum = 0; cardNum < this.cards.length; cardNum++) {
+		this.cards[cardNum].draw(context);
+	}
 }
 
-DeckDisplay.prototype.mouseClick=function(context, canvasRect){
-	console.log(event.clientX - canvasRect.left - this.position.x);
-	console.log(event.clientY - canvasRect.top - this.position.y);
+DeckDisplay.prototype.drawLinks = function(context) {
+	for(var linkNum = 0; linkNum < this.cardLinks.length; linkNum++) {
+		var cardLink = this.cardLinks[linkNum];
+		var cardPair = [];
+
+		for(var cardNum = 0; cardNum < this.cards.length; cardNum++) {
+			var id = this.cards[cardNum].uniqueId;
+			if(id == cardLink[0] || id == cardLink[1]) {
+				cardPair.push(this.cards[cardNum]);
+				if(cardPair.length >= 2) {
+					break;
+				}
+			}
+		}
+
+		if(cardPair.length == 2) {
+			this.drawLink(cardPair[0].getRealPosition().getCenter(), cardPair[1].getRealPosition().getCenter(), context);
+		}
+	}
+
+	if(this.newLinkStartCard != null) {
+		this.drawLink(this.newLinkStartCard.getRealPosition().getCenter(), this.dragMousePos, context);
+		context.fillStyle = this.cardLinkColor;
+		context.fillRect(this.dragMousePos[0] - this.linkSize, this.dragMousePos[1] - this.linkSize,
+			this.linkSize * 2, this.linkSize * 2);
+	}
+}
+
+
+//-------------------------------------------------------------
+//-----------------------Mouse Listeners-----------------------
+//-------------------------------------------------------------
+
+DeckDisplay.prototype.mouseClick=function(e, canvasRect){
+	e.preventDefault();
 }
 
 DeckDisplay.prototype.onMouseDown = function(e, canvasRect) {
+	e.preventDefault();
 
+	var xClickPos = (event.clientX - canvasRect.left);
+	var yClickPos = (event.clientY - canvasRect.top);
+	this.dragMousePos = [xClickPos, yClickPos];
+	
+	// Loop backwards so that with overlapping cards,
+	// we select the card on top (which feels more natural).
+	for(var cardNum = this.cards.length - 1; cardNum >= 0 ; cardNum--) {
+		if(this.cards[cardNum].getRealPosition().contains(xClickPos, yClickPos)) {
+			if(e.which == 1) { // Left Click
+				this.selectCard(cardNum, [xClickPos, yClickPos]);
+				break;
+			} else if (e.which == 3) { // Right Click
+				this.newLinkStartCard = this.cards[cardNum];
+			}
+		}
+	}	
 }
 
 DeckDisplay.prototype.onMouseUp = function(e, canvasRect) {
+	e.preventDefault();
+	
+	if(this.selectedCard != null && e.which == 1) {
+		this.clearSelectedCard();	
+	} if(this.newLinkStartCard != null && e.which == 3) {
+		
+		var xClickPos = (event.clientX - canvasRect.left);
+		var yClickPos = (event.clientY - canvasRect.top);
 
+		var newLinkEndCard = null;
+
+		// Loop backwards so that with overlapping cards,
+		// we select the card on top (which feels more natural).
+		for(var cardNum = this.cards.length - 1; cardNum >= 0 ; cardNum--) {
+			if(this.cards[cardNum].uniqueId == this.newLinkStartCard.uniqueId) {
+				continue;
+			}
+			if(this.cards[cardNum].getRealPosition().contains(xClickPos, yClickPos)) {
+				newLinkEndCard = this.cards[cardNum];
+				break;
+			}
+		}
+
+		if(newLinkEndCard != null) {
+			this.addCardLink(this.newLinkStartCard, newLinkEndCard);
+		}
+
+		this.newLinkStartCard = null;	
+	}
 }
 
 DeckDisplay.prototype.onMouseDrag = function(e, canvasRect) {
-	var xClickPos = event.clientX - canvasRect.left;
-	var yClickPos = event.clientY - canvasRect.top;
+	e.preventDefault();
 
-	console.log("Deck Mouse Drag Pos: (" + xClickPos + ", " + yClickPos + ")!");
+	var xClickPos = (event.clientX - canvasRect.left);
+	var yClickPos = (event.clientY - canvasRect.top);
+	this.dragMousePos = [xClickPos, yClickPos];
+
+	if(this.selectedCard != null) {
+		var newCardX = xClickPos + this.selectedCardPointerOffset[0];
+		var newCardY = yClickPos + this.selectedCardPointerOffset[1];
+
+		this.selectedCard.moveTo(newCardX, newCardY, this.position);
+	}
 }
 
 DeckDisplay.prototype.onMouseWheel = function(e, canvasRect) {
 	var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-	console.log("DeckDisplay Wheel: " + delta);
+	this.adjustScale(delta * this.scaleChangeAmt);
+}
+
+//-------------------------------------------------------------
+//----------------------Helper Functions-----------------------
+//-------------------------------------------------------------
+
+DeckDisplay.prototype.addCard = function(card) {
+	card.scale = this.defaultCardScale;
+	this.cards.push(card);
+}
+
+DeckDisplay.prototype.selectCard = function(cardIndex, pointerPos) {
+	if(this.selectedCard != null) {
+		this.clearSelectedCard();
+	}
+
+	var card = this.cards[cardIndex];
+
+	// Move selected card to back of the cards list (so it always appears on top)
+	this.cards[cardIndex] = this.cards[this.cards.length - 1];
+	this.cards[this.cards.length - 1] = card;
+	
+	this.selectedCard = card;
+	this.selectedCardPointerOffset = [card.getScaledXPos() - pointerPos[0], card.getScaledYPos() - pointerPos[1]];
+	card.shadowSize += this.selectedShadowAddition;
+	card.scale += this.selectedScaleAddition;
+}
+
+DeckDisplay.prototype.clearSelectedCard = function() {
+	if(this.selectedCard != null) {
+		this.selectedCard.shadowSize -= this.selectedShadowAddition;
+		this.selectedCard.scale -= this.selectedScaleAddition;
+		this.selectedCard = null;
+		this.selectedCardPointerOffset = [0, 0];
+	}
+}
+
+DeckDisplay.prototype.adjustScale = function(amt) {
+	// Check bounds
+	var newScale = this.defaultCardScale + amt;
+	if(newScale > this.scaleMax || newScale < this.scaleMin) {
+		return;
+	}
+
+	this.defaultCardScale = newScale;
+	for(var cardNum=0; cardNum<this.cards.length; cardNum++) {
+		//var pos = this.cards[cardNum].getRealPosition();
+		this.cards[cardNum].scale += amt;
+		//this.cards[cardNum].moveTo(pos.x, pos.y, null);
+	}
+}
+
+DeckDisplay.prototype.addCardLink = function(start, end){
+
+	// Check it isn't a duplicate link
+	for(var linkNum = 0; linkNum < this.cardLinks.length; linkNum++) {
+		var cardLink = this.cardLinks[linkNum];
+		if( (cardLink[0] == start.uniqueId && cardLink[1] == end.uniqueId)
+			|| (cardLink[0] == end.uniqueId && cardLink[1] == start.uniqueId) ) {
+			console.log("Link already exists.");
+			return;
+		}
+	}
+
+	this.cardLinks.push([start.uniqueId, end.uniqueId]);
+}
+
+DeckDisplay.prototype.drawLink = function(center1, center2, context) {
+	
+	/*
+	 * Straight Line
+	 */
+	context.beginPath();
+	context.moveTo(center1[0], center1[1]);
+	context.lineTo(center2[0], center2[1]);
+	context.lineWidth = this.linkSize;
+	context.stroke();
+
+
+	/**
+	 * Horizontal and Vertical Lines Only
+	 *
+	// Sort left from right
+	var leftCardCenter;
+	var rightCardCenter;
+	if(center1[0] < center2[0]) {
+		leftCardCenter = center1;
+		rightCardCenter = center2;
+	} else {
+		leftCardCenter = center2;
+		rightCardCenter = center1;
+	}
+
+	// Draw the link
+	context.fillStyle = this.cardLinkColor;
+
+	// Horizontal Component
+	context.fillRect(
+		leftCardCenter[0],
+		leftCardCenter[1] - this.linkSize / 2,
+		rightCardCenter[0] - leftCardCenter[0],
+		this.linkSize
+	);
+
+	// Vertical Component
+	context.fillRect(
+		rightCardCenter[0] - this.linkSize / 2,
+		leftCardCenter[1],
+		this.linkSize,
+		rightCardCenter[1] - leftCardCenter[1]
+	);
+	 *
+	 *
+	**/
 }
