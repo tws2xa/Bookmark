9a40b99f56ed7c99599962c040adaea25d349c7e
enwint <enwinters@comcast.net>
2015-11-09 15:29:03 -0500

    a black canvas covers the board when a state that is not "challenge" or "beingChallenged" is manually set in boardDisplay

--- js/BoardDisplay.js
+++ js/BoardDisplay.js
@@ -8,8 +8,51 @@ function BoardDisplay(x, y, width, height){
 	this.unitwidth = width/4 - 3;
 	
 	this.elements = [];
+
+	//state variables
+	this.currentState = 0;
+	this.doNothing = 0;
+	this.challenge = 1;
+	this.move = 2;
+	this.makeChain = 3;
+	this.beingChallenged = 4;
+
+	
 	
 }
+
+BoardDisplay.prototype.setState =function(newStateNum) {
+	var valid = true;
+	if(newStateNum == this.doNothing) {
+		document.getElementById("chalCanvas").style.visibility="hidden";
+		 document.getElementById("canvas").style.visibility="visible";
+	
+	} else if(newStateNum == this.challenge) {
+		document.getElementById("chalCanvas").style.visibility="visible";
+		 document.getElementById("canvas").style.visibility="hidden";
+
+	} else if(newStateNum == this.move) {
+		document.getElementById("chalCanvas").style.visibility="hidden";
+		 document.getElementById("canvas").style.visibility="visible";
+
+	} else if(newStateNum == this.makeChain) {
+			 document.getElementById("chalCanvas").style.visibility="hidden";
+		 document.getElementById("canvas").style.visibility="visible";
+
+	} else if(newStateNum == this.beingChallenged) {
+		 document.getElementById("chalCanvas").style.visibility="visible";
+		 document.getElementById("canvas").style.visibility="hidden";
+	}
+	 else {
+		console.log("Error - Unrecognized state: " + newStateNum);
+		valid = false;
+	}
+
+	if(valid) {
+		this.currentState = newStateNum;
+	}
+}
+
 
 BoardDisplay.prototype.createBoard = function(){
 	var cards = getBoard();
@@ -24,6 +67,7 @@ BoardDisplay.prototype.createBoard = function(){
 
 BoardDisplay.prototype.draw = function(context){
 	context.fillStyle = this.backgroundColor;
+	this.setState(this.beingChallenged);
 	context.fillRect(this.position.left, this.position.top, this.position.width, this.position.height);
 	for (var r=0; r<4; ++r){
 		for (var c=0; c<4; ++c){
--- js/BoardView.js
+++ js/BoardView.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 	var canvas = $("#canvas")[0];
 	var context = canvas.getContext("2d");
-	var lastClicked;
+	
+
+	var chalCanvas = $("#chalCanvas")[0];
+	var chalContext = chalCanvas.getContext("2d");
+
+var lastClicked;
 	var canvasRect;	
+	var chalCanvasRect;
 	var vMargin = 0.02;
 	var hMargin = 0.015;
 	var clickableGrid;
@@ -11,30 +17,44 @@ $(document).ready(function() {
 	var canvasHeight = window.innerHeight * 0.935;
 	var boardDisplay;
 	var teamsDisplay;
-	
+
+	var scaledHMargin = canvasWidth * hMargin;
+		var scaledVMargin = canvasHeight * vMargin;
+		var upperPos = canvasHeight * 0.01;
+		var leftPos = scaledHMargin;
+		var rightPos = canvasWidth - scaledHMargin;
+
+	//state variables
+	this.currentState = 0;
+	this.doNothing = 0;
+	this.challenge = 1;
+	this.move = 2;
+	this.makeChain = 3;
+	this.beingChallenged = 4;
+
+	setState(this.move);
 	setCanvasSize();
 	init();
 	
 	function startTimer() {
 	var timer = setInterval(updateBoard, 5000);
 	console.log("in timer");
-}
+	
+	}
 	
 	
 	function setCanvasSize() {
 		canvas.width = canvasWidth;
 		canvas.height = canvasHeight;
+		chalCanvas.width = canvasWidth;
+		chalCanvas.height = canvasHeight;
 	}
 	
 	function init() {
 		if(typeof game_loop != "undefined") clearInterval(game_loop);
 		game_loop = setInterval(paint, 60);
 		
-		var scaledHMargin = canvasWidth * hMargin;
-		var scaledVMargin = canvasHeight * vMargin;
-		var upperPos = canvasHeight * 0.01;
-		var leftPos = scaledHMargin;
-		var rightPos = canvasWidth - scaledHMargin;
+		
 	
 
 		canvasRect = canvas.getBoundingClientRect();
@@ -42,6 +62,12 @@ $(document).ready(function() {
 		canvas.addEventListener("mousedown", onMouseDown);
 		canvas.addEventListener("mouseup", onMouseUp);
 		
+		chalCanvasRect = chalCanvas.getBoundingClientRect();
+		chalCanvas.addEventListener("click", onClick);
+		chalCanvas.addEventListener("mousedown", onMouseDown);
+		chalCanvas.addEventListener("mouseup", onMouseUp);
+
+
 		boardDisplay = new BoardDisplay(
 			leftPos,
 			upperPos,
@@ -49,7 +75,7 @@ $(document).ready(function() {
 			canvasHeight*0.95
 		);
 		boardDisplay.createBoard();
-		
+	
 		
 		teamsDisplay = new TeamsDisplay(
 			leftPos+canvasWidth*.82,
@@ -68,11 +94,22 @@ $(document).ready(function() {
 			// Firefox
 			canvas.addEventListener("DOMMouseScroll", onMouseWheel);
 		}
+		if (chalCanvas.addEventListener) {
+			// IE9, Chrome, Safari, Opera
+			chalCanvas.addEventListener("mousewheel", onMouseWheel);
+			// Firefox
+			chalCanvas.addEventListener("DOMMouseScroll", onMouseWheel);
+		}
 
 		// Prevent context menu appearing on right click
 		canvas.oncontextmenu = function(e) {
 			return false;
 		}
+		chalCanvas.oncontextmenu = function(e) {
+			return false;
+		}
+
+	
 	startTimer();
 	}
 	
@@ -80,10 +117,16 @@ $(document).ready(function() {
 		// Clear Screen
 		context.fillStyle = "#9ec7d3";
 		context.fillRect(0, 0, canvasWidth, canvasHeight);
-
+		chalContext.fillStyle = "#000000";
+		chalContext.fillRect(leftPos,
+			upperPos,
+			canvasWidth*0.80,
+			canvasHeight*0.95);
 		// Draw Card
 		boardDisplay.draw(context);
 		teamsDisplay.draw(context);
+		teamsDisplay.draw(chalContext);
+		 
 	}
 
 	function updateBoard(){
@@ -148,5 +191,37 @@ function argsDisplayMouseDrag(event) {
 		argumentDisplay.onMouseDrag(event, canvasRect);
 	}
 */
+
+function setState(newStateNum) {
+	var valid = true;
+	if(newStateNum == this.doNothing) {
+		document.getElementById("chalCanvas").style.visibility="hidden";
+		 document.getElementById("canvas").style.visibility="visible";
+	
+	} else if(newStateNum == this.challenge) {
+		document.getElementById("chalCanvas").style.visibility="visible";
+		 document.getElementById("canvas").style.visibility="hidden";
+
+	} else if(newStateNum == this.move) {
+		document.getElementById("chalCanvas").style.visibility="hidden";
+		 document.getElementById("canvas").style.visibility="visible";
+
+	} else if(newStateNum == this.makeChain) {
+			 document.getElementById("chalCanvas").style.visibility="hidden";
+		 document.getElementById("canvas").style.visibility="visible";
+
+	} else if(newStateNum == this.beingChallenged) {
+		 document.getElementById("chalCanvas").style.visibility="visible";
+		 document.getElementById("canvas").style.visibility="hidden";
+	}
+	 else {
+		console.log("Error - Unrecognized state: " + newStateNum);
+		valid = false;
+	}
+
+	if(valid) {
+		this.currentState = newStateNum;
+	}
+}
 
 });
