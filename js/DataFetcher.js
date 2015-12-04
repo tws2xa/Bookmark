313a30415ef6a6c6ae94a780a6b5be6d29c832bf
Thomas Sparks <tws2xa@virginia.edu>
2015-12-04 13:14:11 -0500

    Change target url for data fetcher

--- js/DataFetcher.js
+++ js/DataFetcher.js
@@ -59,7 +59,7 @@ for(var i=0; i<15; i++){
 
 /* --------------------- Above generates data for testing purposes --------------------- */
 
-var BASE_URL = "http://localhost:8080/Bookmark/bookmark/";
+var BASE_URL = "http://gdrg.cs.virginia.edu:8080/Bookmark/bookmark/";
 var BEGIN_SESSION = "begin-session";
 var JOIN_SESSION = "join-session";
 var LOGIN = "login";
